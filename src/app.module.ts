0ea0c4713a6374f28d974e3ebf01ee2c449a7ff6
MatthewsGomez <matthewsgomes0330@gmail.com>
2024-05-20 23:51:59 -0500

    Creacion de recurso users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 import { TypeOrmModule }  from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -11,9 +13,9 @@ import { TypeOrmModule }  from '@nestjs/typeorm';
     username: 'root',
     password: '',
     database: 'apicul',
-    entities: [],
+    entities: [User],
     synchronize: true,
-  }),],
+  }), UserModule,],
   controllers: [AppController],
   providers: [AppService],
 })
